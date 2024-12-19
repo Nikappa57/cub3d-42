fc68462e1bc7cc7b44ad53fd6fc6eaa404e8cf16
Leonardo Ottavi <leonardottavi02@gmail.com>
2024-12-19 16:43:28 +0100

    norm 3

--- srcs/main/init.c
+++ srcs/main/init.c
@@ -6,59 +6,51 @@
 /*   By: lottavi <lottavi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 18:27:42 by lgaudino          #+#    #+#             */
-/*   Updated: 2024/12/19 16:08:25 by lottavi          ###   ########.fr       */
+/*   Updated: 2024/12/19 16:43:03 by lottavi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-void *ft_calloc(size_t count, size_t size)
+void	*ft_calloc(size_t count, size_t size)
 {
-	void *ptr = malloc(count * size);
+	void	*ptr;
+
+	ptr = malloc(count * size);
 	if (!ptr)
 	{
 		perror("Error allocating memory");
-		return NULL;
+		return (NULL);
 	}
 	memset(ptr, 0, count * size);
-	return ptr;
+	return (ptr);
 }
 
-static int init_mlx(t_mlx *mlx)
+static int	init_mlx(t_mlx *mlx)
 {
 	mlx->mlx = mlx_init();
 	if (!mlx->mlx)
-	{
-		fprintf(stderr, "Error: mlx_init failed\n");
-		return -1;
-	}
+		return (printf("Error: mlx_init failed\n"), (-1));
 	mlx->win = mlx_new_window(mlx->mlx, WIN_WIDTH, WIN_HEIGHT, WIN_TITLE);
 	if (!mlx->win)
-	{
-		fprintf(stderr, "Error: mlx_new_window failed\n");
-		return -1;
-	}
+		return (printf("Error: mlx_new_window failed\n"), (-1));
 	mlx->data.img = mlx_new_image(mlx->mlx, WIN_WIDTH, WIN_HEIGHT);
 	if (!mlx->data.img)
-	{
-		fprintf(stderr, "Error: mlx_new_image failed\n");
-		return -1;
-	}
-	mlx->data.addr = mlx_get_data_addr(mlx->data.img, &mlx->data.bits_per_pixel, &mlx->data.line_length, &mlx->data.endian);
+		return (printf("Error: mlx_new_image failed\n"), (-1));
+	mlx->data.addr = mlx_get_data_addr(mlx->data.img,
+			&mlx->data.bits_per_pixel, &mlx->data.line_length,
+			&mlx->data.endian);
 	if (!mlx->data.addr)
-	{
-		fprintf(stderr, "Error: mlx_get_data_addr failed\n");
-		return -1;
-	}
-	return 0;
+		return (printf("Error: mlx_get_data_addr failed\n"), (-1));
+	return (0);
 }
 
-static int init_map(t_map *map, const char *map_path)
+static int	init_map(t_map *map, const char *map_path)
 {
-	char line[256];
-	int width = 0;
-	int height = 0;
-	FILE *file = fopen(map_path, "r");
+	char	line[256];
+	int		width = 0;
+	int		height = 0;
+	FILE	*file = fopen(map_path, "r");
 	while (fgets(line, sizeof(line), file))
 	{
 		int len = strlen(line);
@@ -103,7 +95,8 @@ static int init_map(t_map *map, const char *map_path)
 	return 0;
 }
 
-static void set_position_and_direction(t_state *state, char direction_char, int col, int row)
+static void	set_position_and_direction(t_state *state,
+	char direction_char, int col, int row)
 {
 	state->pos.x = col + 0.5;
 	state->pos.y = row + 0.5;
@@ -117,27 +110,34 @@ static void set_position_and_direction(t_state *state, char direction_char, int 
 		get_dir_v(&state->dir, RIGHT);
 }
 
-static void parse_map(t_state *state, const char *map_path)
+static void	parse_map(t_state *state, const char *map_path)
 {
-	FILE *file = fopen(map_path, "r");
-	char line[256];
-	int row = 0;
+	FILE	*file;
+	char	line[256];
+	int		row;
+	size_t	col;
+
+	file = fopen(map_path, "r");
+	row = 0;
 	while (fgets(line, sizeof(line), file))
 	{
-		for (size_t col = 0; col < strlen(line); col++)
+		col = 0;
+		while (col < strlen(line))
 		{
-			if (line[col] == 'N' || line[col] == 'S' || line[col] == 'W' || line[col] == 'E')
+			if (line[col] == 'N' || line[col] == 'S'
+				|| line[col] == 'W' || line[col] == 'E')
 			{
 				set_position_and_direction(state, line[col], col, row);
-				break;
+				break ;
 			}
+			col++;
 		}
 		row++;
 	}
 	fclose(file);
 }
 
-static int init_state(t_state *state, const char *map_path)
+static int	init_state(t_state *state, const char *map_path)
 {
 	parse_map(state, map_path);
 
@@ -146,43 +146,46 @@ static int init_state(t_state *state, const char *map_path)
 	state->rot = NONE_ROT;
 	v_perp(&state->plane, state->dir);
 	v_mul(&state->plane, state->plane, tan(FOV / 2));
-	return 0;
+	return (0);
 }
 
-static int load_texture(t_cub3d *cube, t_img *t, const char *path)
+static int	load_texture(t_cub3d *cube, t_img *t, const char *path)
 {
 	t->img = mlx_xpm_file_to_image(cube->mlx.mlx, (char *)path, &t->img_width, &t->img_height);
 	if (!t->img)
 	{
 		fprintf(stderr, "Error: Failed to load texture %s\n", path);
-		return -1;
+		return (-1);
 	}
 	t->addr = mlx_get_data_addr(t->img, &t->bits_per_pixel, &t->line_length, &t->endian);
 	if (!t->addr)
-		return -1;
-	return 0;
+		return (-1);
+	return (0);
 }
 
-static int init_textures(t_cub3d *cube)
+static int	init_textures(t_cub3d *cube)
 {
+	int	i;
+
 	const char *textures[] = {
 		"./texture/wolfenstein/bluestone.xpm",
 		"./texture/wolfenstein/colorstone.xpm",
 		"./texture/wolfenstein/eagle.xpm",
 		"./texture/wolfenstein/redbrick.xpm"
 	};
-
-	for (int i = 0; i < 4; i++)
+	i = 0;
+	while (i < 4)
 	{
 		if (load_texture(cube, &cube->texture[i], textures[i]) == -1)
-			return -1;
+			return (-1);
+		i++;
 	}
 	cube->ceiling_color = BLUE2;
 	cube->floor_color = GREEN;
-	return 0;
+	return (0);
 }
 
-void init_cube(t_cub3d *cube, const char *map_path)
+void	init_cube(t_cub3d *cube, const char *map_path)
 {
 	ft_bzero(cube, sizeof(t_cub3d));
 	if (init_state(&cube->state, map_path) == -1)
