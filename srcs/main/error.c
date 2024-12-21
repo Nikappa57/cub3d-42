36ed457fe7b4a9bd8babee143bd5b9ea5d87d16b
Leonardo Ottavi <leonardottavi02@gmail.com>
2024-12-22 00:28:17 +0100

    bbv

--- srcs/main/error.c
+++ srcs/main/error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgaudino <lgaudino@student.42roma.it>      +#+  +:+       +#+        */
+/*   By: lottavi <lottavi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 20:24:01 by lgaudino          #+#    #+#             */
-/*   Updated: 2024/12/20 14:42:47 by lgaudino         ###   ########.fr       */
+/*   Updated: 2024/12/22 00:27:44 by lottavi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	exit_error(t_cub3d *cube, char *message)
 {
-	ft_putstr_fd("Error: ", 2);
+	ft_putstr_fd("Error\n", 2);
 	ft_putstr_fd(message, 2);
 	ft_putchar_fd('\n', 2);
 	clear_exit(cube, 1);
