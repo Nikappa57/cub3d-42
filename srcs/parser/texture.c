859538432436ab65f28ea17a26f6d0452a54f3c1
Leonardo Ottavi <leonardottavi02@gmail.com>
2024-12-21 18:23:49 +0100

    nomr2

--- srcs/parser/texture.c
+++ srcs/parser/texture.c
@@ -6,7 +6,7 @@
 /*   By: lottavi <lottavi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 10:57:55 by lottavi           #+#    #+#             */
-/*   Updated: 2024/12/21 18:19:28 by lottavi          ###   ########.fr       */
+/*   Updated: 2024/12/21 18:21:35 by lottavi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 int	parse_color(const char *str)
 {
-	int	r, g, b;
+	int	r;
+	int	g;
+	int	b;
 
 	if (!str)
 		return (-1);
@@ -65,51 +67,70 @@ char	*allocate_and_copy(const char *src)
 	return (dest);
 }
 
+int	open_config_file(const char *file_path)
+{
+	int	fd;
+
+	fd = open(file_path, O_RDONLY);
+	if (fd == -1)
+		printf("\033[0;31mError: Failed to open config file %s\033[0m\n", file_path);
+	return (fd);
+}
+
+char	**split_and_trim_line(char *line)
+{
+	char	*trimmed_line;
+	char	**split_line;
+
+	trimmed_line = allocate_and_copy(line);
+	if (!trimmed_line)
+		return (NULL);
+	trimmed_line = skip_spaces_and_tabs(trimmed_line);
+	if (!trimmed_line)
+		return (NULL);
+	split_line = ft_split(trimmed_line, ' ');
+	return (split_line);
+}
+
+void	parse_config_line(char **split_line, t_config *config, int *i)
+{
+	if (split_line[0] && split_line[1])
+	{
+		if (ft_strcmp(split_line[0], "NO") == 0)
+			config->north_texture = allocate_and_copy(split_line[1]), (*i)++;
+		else if (ft_strcmp(split_line[0], "SO") == 0)
+			config->south_texture = allocate_and_copy(split_line[1]), (*i)++;
+		else if (ft_strcmp(split_line[0], "WE") == 0)
+			config->west_texture = allocate_and_copy(split_line[1]), (*i)++;
+		else if (ft_strcmp(split_line[0], "EA") == 0)
+			config->east_texture = allocate_and_copy(split_line[1]), (*i)++;
+		else if (ft_strcmp(split_line[0], "F") == 0)
+			config->floor_color = parse_color(split_line[1]), (*i)++;
+		else if (ft_strcmp(split_line[0], "C") == 0)
+			config->ceiling_color = parse_color(split_line[1]), (*i)++;
+	}
+}
+
 int	read_config(const char *file_path, t_config *config)
 {
 	int		fd;
 	char	*line;
 	char	**split_line;
-	char	*trimmed_line;
-	int		i = 0;
+	int		i;
 
+	i = 0;
 	if (!file_path || !config)
 		return (-1);
-
-	fd = open(file_path, O_RDONLY);
+	fd = open_config_file(file_path);
 	if (fd == -1)
 		return (-1);
-
 	while ((line = get_next_line(fd)) != NULL)
 	{
-		trimmed_line = allocate_and_copy(line);
-		if (!trimmed_line)
-			continue;
-
-		trimmed_line = skip_spaces_and_tabs(trimmed_line);
-		if (!trimmed_line)
-			continue;
-
-		split_line = ft_split(trimmed_line, ' ');
+		split_line = split_and_trim_line(line);
 		if (!split_line)
 			continue;
-
-		if (split_line[0] && split_line[1])
-		{
-			if (ft_strcmp(split_line[0], "NO") == 0)
-				config->north_texture = allocate_and_copy(split_line[1]), i++;
-			else if (ft_strcmp(split_line[0], "SO") == 0)
-				config->south_texture = allocate_and_copy(split_line[1]), i++;
-			else if (ft_strcmp(split_line[0], "WE") == 0)
-				config->west_texture = allocate_and_copy(split_line[1]), i++;
-			else if (ft_strcmp(split_line[0], "EA") == 0)
-				config->east_texture = allocate_and_copy(split_line[1]), i++;
-			else if (ft_strcmp(split_line[0], "F") == 0)
-				config->floor_color = parse_color(split_line[1]), i++;
-			else if (ft_strcmp(split_line[0], "C") == 0)
-				config->ceiling_color = parse_color(split_line[1]), i++;
-		}
-		if(i == 6)
+		parse_config_line(split_line, config, &i);
+		if (i == 6)
 			break;
 	}
 	close(fd);
