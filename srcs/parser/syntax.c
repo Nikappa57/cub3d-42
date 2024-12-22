ce948b69e52f11b6f9c57d23abd82dd719111282
Leonardo Ottavi <leonardottavi02@gmail.com>
2024-12-22 01:19:14 +0100

    Refactor cub file validation logic and enhance error handling for floor and ceiling colors

--- srcs/parser/syntax.c
+++ srcs/parser/syntax.c
@@ -6,7 +6,7 @@
 /*   By: lottavi <lottavi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 21:09:20 by lottavi           #+#    #+#             */
-/*   Updated: 2024/12/22 00:25:12 by lottavi          ###   ########.fr       */
+/*   Updated: 2024/12/22 01:18:48 by lottavi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,63 +51,83 @@ int has_extra_characters(const char *line) {
     return 0;
 }
 
-// Funzione principale per validare il file .cub
-int validate_cub_file(const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        perror("Errore nell'apertura del file");
+int validate_floor_color(const char *line, int *found_f) {
+    if (has_extra_characters(line) || !is_valid_color(line)) {
+        printf("Errore: colore del pavimento (F) non valido o contiene caratteri extra.\n");
         return 0;
     }
+    (*found_f)++;
+    return 1;
+}
 
-    char line[MAX_LINE_LENGTH];
-    int found_no = 0, found_so = 0, found_we = 0, found_ea = 0, found_f = 0, found_c = 0;
-
-    while (fgets(line, MAX_LINE_LENGTH, file)) {
-        line[strcspn(line, "\n")] = '\0';
-
-        if (strncmp(line, "NO ", 3) == 0) {
-            found_no++;
-        } else if (strncmp(line, "SO ", 3) == 0) {
-            found_so++;
-        } else if (strncmp(line, "WE ", 3) == 0) {
-            found_we++;
-        } else if (strncmp(line, "EA ", 3) == 0) {
-            found_ea++;
-        } else if (strncmp(line, "F ", 2) == 0) {
-            if (has_extra_characters(line + 2) || !is_valid_color(line + 2)) {
-                printf("Errore: colore del pavimento (F) non valido o contiene caratteri extra.\n");
-                fclose(file);
-                return 0;
-            }
-            found_f++;
-        } else if (strncmp(line, "C ", 2) == 0) {
-            if (has_extra_characters(line + 2) || !is_valid_color(line + 2)) {
-                printf("Errore: colore del soffitto (C) non valido o contiene caratteri extra.\n");
-                fclose(file);
-                return 0;
-            }
-            found_c++;
-        }
-		else if (*line == '\n' || *line == '\0')
-			continue;
-		else
-		{
-			if (line[0] != '1' && line[0] != '0' && line[0] != ' ' && line[0] != 'N' && line[0] != 'S' && line[0] != 'W' && line[0] != 'E')
-            {
-				printf("Errore: linea non riconosciuta o fuori ordine: '%s'\n", line);
-            	fclose(file);
-            	return 0;
-			}
-			printf("Linea: %s\n", line);
-			break;
-        }
+int validate_ceiling_color(const char *line, int *found_c) {
+    if (has_extra_characters(line) || !is_valid_color(line)) {
+        printf("Errore: colore del soffitto (C) non valido o contiene caratteri extra.\n");
+        return 0;
     }
-    fclose(file);
-	printf("NO: %d\nSO: %d\nWE: %d\nEA: %d\nF: %d\nC: %d\n", found_no, found_so, found_we, found_ea, found_f, found_c);
+    (*found_c)++;
+    return 1;
+}
+
+int validate_map_line(const char *line) {
+    if (line[0] != '1' && line[0] != '0' && line[0] != ' ' && line[0] != 'N' && line[0] != 'S' && line[0] != 'W' && line[0] != 'E') {
+        printf("Errore: linea non riconosciuta o fuori ordine: '%s'\n", line);
+        return 0;
+    }
+    return 1;
+}
+
+int check_directives_count(int found_no, int found_so, int found_we, int found_ea, int found_f, int found_c) {
+    printf("NO: %d\nSO: %d\nWE: %d\nEA: %d\nF: %d\nC: %d\n", found_no, found_so, found_we, found_ea, found_f, found_c);
     if (found_no != 1 || found_so != 1 || found_we != 1 || found_ea != 1 || found_f != 1 || found_c != 1) {
         printf("Errore: mancano alcune direttive obbligatorie o sono duplicate.\n");
         return 0;
     }
-
     return 1;
+}
+
+int validate_line(const char *line, int *found_no, int *found_so, int *found_we, int *found_ea, int *found_f, int *found_c) {
+    if (strncmp(line, "NO ", 3) == 0) {
+        (*found_no)++;
+    } else if (strncmp(line, "SO ", 3) == 0) {
+        (*found_so)++;
+    } else if (strncmp(line, "WE ", 3) == 0) {
+        (*found_we)++;
+    } else if (strncmp(line, "EA ", 3) == 0) {
+        (*found_ea)++;
+    } else if (strncmp(line, "F ", 2) == 0) {
+        if (!validate_floor_color(line + 2, found_f)) return 0;
+    } else if (strncmp(line, "C ", 2) == 0) {
+        if (!validate_ceiling_color(line + 2, found_c)) return 0;
+    } else if (*line == '\0' || *line == '\n') {
+        return 1;
+    } else {
+        if (!validate_map_line(line)) return 0;
+    }
+    return 1;
+}
+
+
+int validate_cub_file(const char *filename) {
+    int fd = open(filename, O_RDONLY);
+    if (fd < 0) {
+        perror("Errore nell'apertura del file");
+        return 0;
+    }
+
+    char *line;
+    int found_no = 0, found_so = 0, found_we = 0, found_ea = 0, found_f = 0, found_c = 0;
+
+    while ((line = get_next_line(fd)) != NULL) {
+        line[strcspn(line, "\n")] = '\0';
+        if (!validate_line(line, &found_no, &found_so, &found_we, &found_ea, &found_f, &found_c)) {
+            free(line);
+            close(fd);
+            return 0;
+        }
+        free(line);
+    }
+
+    close(fd);
+    return check_directives_count(found_no, found_so, found_we, found_ea, found_f, found_c);
 }
